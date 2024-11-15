00a970af16f1d7a12f35c43b87b5b34e94b37cd2
kodicat <yevhenii.yukhymchuk@gmail.com>
2024-11-15 05:44:53 +0200

    Save orders in database

---
+++ src/routes/checkout/+page.server.ts
@@ -0,0 +1,35 @@
+import { fail, json, type Actions } from '@sveltejs/kit';
+import { createAnonDatabaseClient } from '$lib/appwrite';
+import { PUBLIC_APPWRITE_DATABASE, PUBLIC_APPWRITE_ORDERS_COLLECTION } from '$env/static/public';
+
+export const actions: Actions = {
+    default: async ({ request, locals }) => {
+        const userId = locals.user?.$id;
+
+        const formData = await request.formData();
+        const phone = formData.get('phone') as string;
+        const address = formData.get('address') as string;
+        const cartProducts = formData.get('cartProducts') as string;
+        const totalAmount = parseFloat(formData.get('totalAmount') as string);
+
+        try {
+            const orderData = {
+                userId,
+                phone,
+                address,
+                totalAmount,
+                cartProducts
+            };
+
+            const { databases } = createAnonDatabaseClient();
+            const order = await databases.createDocument(PUBLIC_APPWRITE_DATABASE, PUBLIC_APPWRITE_ORDERS_COLLECTION, 'unique()', orderData);
+
+            console.log(order);
+
+            return { success: true, orderId: order.$id };
+        } catch (error) {
+            console.error('Failed to save order:', error);
+            return fail(400, { success: false, error: 'Failed to save order', cause: error });
+        }
+    }
+}
--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import type { CartProduct } from '$lib/types';
+    import { onMount } from 'svelte';
 
     let cartProducts = $state<CartProduct[]>([]);
     let name = $state<string>('');
     let phone = $state<string>('');
     let address = $state<string>('');
-
-
     let errorMessage = $state<string>('');
+    let totalAmount = $state<string>('');
 
-    // should be browser
-    if (typeof window !== 'undefined') {
-        const storedCart = localStorage.getItem('cart');
-        if (storedCart) {
-            cartProducts = JSON.parse(storedCart);
+    onMount(() => {
+        if (typeof window !== 'undefined') {
+            const storedCart = localStorage.getItem('cart');
+            if (storedCart) {
+                cartProducts = JSON.parse(storedCart || '[]');
+            }
         }
-    }
+        totalAmount = cartProducts.reduce(
+            (total, product) => total + product.product.price * product.quantity,
+            0
+        ).toFixed(2);
+    });
 
-    function confirmOrder() {
+    async function confirmOrder() {
         if (cartProducts.length === 0) {
             return;
         }
@@ -26,10 +31,8 @@
             errorMessage = 'Введіть обовязкові поля';
             return;
         }
-        // save in db
 
         localStorage.removeItem('cart');
-
         const cartParam = encodeURIComponent(JSON.stringify(cartProducts));
         goto(`/order-success?cart=${cartParam}`);
     }
@@ -37,68 +40,72 @@
 
 <h1 class="text-3xl font-semibold mb-6">Оформлення замовлення</h1>
 
-<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    {#each cartProducts as product}
-        <div class="overflow-hidden rounded-xl bg-white shadow-lg">
-            <img src={product.product.thumbnail} alt={product.product.title} class="h-48 w-full object-cover" />
-            <div class="p-4">
-                <p class="mb-2 font-medium text-lg text-gray-800">{product.product.title}</p>
-                <div class="flex justify-between">
-                    <span class="text-sm text-gray-500">{product.quantity} x {product.product.price} грн.</span>
-                    <span class="text-xl font-semibold text-gray-800">{(product.product.price * product.quantity).toFixed(2)} грн.</span>
+<form method="POST" action="/checkout" onsubmit={() => confirmOrder()}>
+    <input type="hidden" name="cartProducts" value={JSON.stringify(cartProducts)} />
+    <input type="hidden" name="totalAmount" value={totalAmount} />
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {#each cartProducts as product}
+            <div class="overflow-hidden rounded-xl bg-white shadow-lg">
+                <img src={product.product.thumbnail} alt={product.product.title} class="h-48 w-full object-cover" />
+                <div class="p-4">
+                    <p class="mb-2 font-medium text-lg text-gray-800">{product.product.title}</p>
+                    <div class="flex justify-between">
+                        <span class="text-sm text-gray-500">{product.quantity} x {product.product.price} грн.</span>
+                        <span class="text-xl font-semibold text-gray-800">{(product.product.price * product.quantity).toFixed(2)} грн.</span>
+                    </div>
                 </div>
             </div>
+        {/each}
+    </div>
+
+    <div class="mt-6 space-y-4">
+        <div>
+            <label for="name" class="px-2 py-2 block text-gray-700 font-medium">Ім'я</label>
+            <input
+                type="name"
+                id="name"
+                name="name"
+                placeholder="Ім'я замовника"
+                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
+                required
+            />
         </div>
-    {/each}
-</div>
 
-<div class="mt-6 space-y-4">
-    <div>
-        <label for="phone" class="px-2 py-2 block text-gray-700 font-medium">Ім'я</label>
-        <input
-            type="tel"
-            id="phone"
-            bind:value={name}
-            placeholder="Ім'я замовника"
-            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
-            required
-        />
+        <div>
+            <label for="phone" class="px-2 py-2 block text-gray-700 font-medium">Телефон *</label>
+            <input
+                type="phone"
+                id="phone"
+                name="phone"
+                placeholder="Введіть телефон для підтвердження замовлення"
+                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
+                required
+            />
+        </div>
+        
+        <div>
+            <label for="address" class="px-2 py-2 block text-gray-700 font-medium">Адреса доставки *</label>
+            <input
+                type="address"
+                id="address"
+                name="address"
+                placeholder="Повна адреса доставки, місто, вулиця, будинок"
+                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
+                required
+            />
+        </div>
     </div>
 
-    <div>
-        <label for="phone" class="px-2 py-2 block text-gray-700 font-medium">Телефон *</label>
-        <input
-            type="tel"
-            id="phone"
-            bind:value={phone}
-            placeholder="Введіть телефон для підтвердження замовлення"
-            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
-            required
-        />
-    </div>
-    
-    <div>
-        <label for="address" class="px-2 py-2 block text-gray-700 font-medium">Адреса доставки *</label>
-        <input
-            type="text"
-            id="address"
-            bind:value={address}
-            placeholder="Повна адреса доставки, місто, вулиця, будинок"
-            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
-            required
-        />
-    </div>
-</div>
+    {#if errorMessage}<p class="text-red-500 text-sm mt-2">{errorMessage}</p>{/if}
 
-{#if errorMessage}<p class="text-red-500 text-sm mt-2">{errorMessage}</p>{/if}
+    <div class="mt-6 text-lg font-semibold">
+        <div class="flex justify-between mb-4">
+            <span>Загалом до оплати:</span>
+            <span class="text-xl">{cartProducts.reduce((total, product) => total + (product.product.price * product.quantity), 0).toFixed(2)} грн.</span>
+        </div>
 
-<div class="mt-6 text-lg font-semibold">
-    <div class="flex justify-between mb-4">
-        <span>Загалом до оплати:</span>
-        <span class="text-xl">{cartProducts.reduce((total, product) => total + (product.product.price * product.quantity), 0).toFixed(2)} грн.</span>
+        <button type="submit" class="w-full bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition">
+            Підтвердини замовлення
+        </button>
     </div>
-
-    <button onclick={confirmOrder} class="w-full bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition">
-        Підтвердини замовлення
-    </button>
-</div>
+</form>
